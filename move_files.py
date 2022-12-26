b6518c8bc45fd5982cea0de6723f1adab09fa576
Eunie2 <suzuki.y.1019@gmail.com>
2022-12-26 15:14:14 +0900

    remove comments

--- move_files.py
+++ move_files.py
@@ -1,21 +1,8 @@
 import shutil
 import glob
-# import argparse
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument()
-# print("Input and Output folder name?")
-# input_path, output_path = input().split()
-
-# help("Input : ./input_folder/", "Output: ./output_folder/")
 
 input_path = input("input? ")
 output_path = input("output? ")
-
-print("input is a .. "+input_path)
-print("output is a .. "+output_path)
-# input_path = "./input/"
-# output_path = "./output/"
 
 move_file_list = glob.glob(input_path + "*")
 print(move_file_list)
