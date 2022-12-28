418c0d4de1dc2a7aa852a6eaa3f41209994533e2
Eunie2 <suzuki.y.1019@gmail.com>
2022-12-28 13:24:56 +0900

    add 4 options

--- move_files.py
+++ move_files.py
@@ -1,12 +1,43 @@
 import shutil
 import glob
+import os
+import sys
 
-input_path = input("input? ")
-output_path = input("output? ")
+input_mode = input("what is mode? ")
+if input_mode == "create":
+    directory_path = input("directory path to ./path/path2/~~/ ")
+    os.makedirs(directory_path)
+    sys.exit("success!")
 
-move_file_list = glob.glob(input_path + "*")
-print(move_file_list)
+elif input_mode == "copy":
+    input_path = input("input path to ./directory path/file name/? ")
+    output_path = input("output path to ./directory path/file name/? ")
+    shutil.copyfile(input_path, output_path)
+    sys.exit("success!")
 
-for item in move_file_list:
-    shutil.move(item, output_path)
-    print("success")
+elif input_mode == "all_of_the_copy":
+    input_path = input("input path to ./directory path/file name/? ")
+    output_path = input("output path to ./directory path/file name/? ")
+    shutil.copytree(input_path, output_path)
+    sys.exit("success!")
+
+elif input_mode == "delete":
+    input_path = input("input path to ./directory path/file name/? ")
+    yes_or_no = input("i can do delete now. yes or no? ")
+    if yes_or_no == "yes":
+        shutil.rmtree(input_path)
+    sys.exit("success!")
+
+elif input_mode == "move":
+    input_path = input("input ./directory path/? ")
+    output_path = input("output ./directory path/? ")
+
+    move_file_list = glob.glob(input_path + "*")
+    print(move_file_list)
+
+    for item in move_file_list:
+        shutil.move(item, output_path)
+        print("success!")
+
+else:
+    sys.exit("good bye!")
